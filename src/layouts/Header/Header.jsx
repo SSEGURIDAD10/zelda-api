dde8af14f8469c3121c8890adacfa7955e1e0db8
Villagran <luisbmx_haro@hotmail.com>
2024-05-16 11:51:54 -0600

    Header Style 1.0.0

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import './Header.css'
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
+  // 
+  const linkIsActive = (isActive) => {
+    return isActive ? 'header__item-link header__item-link--is--active' : 'header__item-link'
+  }
+
   return (
     <header>
-      <nav className='header'>
-        <a href='/' className='header__logo'>Zelda Lull-Api</a>
-        <ul className='header__nav-list'>
-          <li className='header__list-link'>Home</li>
-          <li className='header__list-item'>Dashboard</li>
+      <nav className="header">
+        <NavLink to="/" className="header__logo">
+          Zelda Lull-Api
+        </NavLink>
+        <ul className="header__nav-list">
+          <li className="header__list-link">
+            <NavLink to="/" className={({isActive})=>linkIsActive(isActive)}>
+              Home
+            </NavLink>
+          </li>
+          <li className="header__list-item">
+            <NavLink to="dashboard" className={({isActive})=>linkIsActive(isActive)}>
+              Dashboard
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
