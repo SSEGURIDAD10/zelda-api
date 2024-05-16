68e928d107f8f64e9f2762f92b3d2815fcd31dbd
Villagran <luisbmx_haro@hotmail.com>
2024-05-16 09:34:58 -0600

    Se la da estilos al Header

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
     <header>
-      <nav>
-        
+      <nav className='header'>
+        <a href='/' className='header__logo'>Zelda Lull-Api</a>
+        <ul className='header__nav-list'>
+          <li className='header__list-link'>Home</li>
+          <li className='header__list-item'>Dashboard</li>
+        </ul>
       </nav>
     </header>
   )
